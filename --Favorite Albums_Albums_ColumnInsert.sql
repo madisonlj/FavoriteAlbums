e7166c7029ad6d67449eaa2801bbbba2905e33a7
Madison W <97496124+madisonlj@users.noreply.github.com>
2022-01-25 20:00:07 -0500

    Add files via upload

---
+++ --Favorite Albums_Albums_ColumnInsert.sql
@@ -0,0 +1,31 @@
+--Albums Table: column inserts
+INSERT INTO albums(album_title, artist_id, artist_name, 
+                   genre, release_date, record_label)
+VALUES
+('Nothing Was the Same', '3', 'Drake',
+  'Hip-Hop/Rap', '2013-09-24', 'Cash Money Records')
+
+INSERT INTO albums(album_title, artist_id, artist_name, 
+                   genre, release_date, record_label)
+VALUES
+('Future Nostalgia', '5', 'Dua Lipa',
+  'Pop', '2020-03-27', 'Warner Records')
+
+INSERT INTO albums(album_title, artist_id, artist_name, 
+                   genre, release_date, record_label)
+VALUES
+('Memoirs of an Imperfect Angel', '1', 'Mariah Carey',
+  'R&B', '2009-09-29', 'Island Records')
+
+INSERT INTO albums(album_title, artist_id, artist_name, 
+                   genre, release_date, record_label)
+VALUES
+('Badlands', '4', 'Halsey',
+  'Alternative', '2015-08-28', 'Astralwerks')
+
+INSERT INTO albums(album_title, artist_id, artist_name, 
+                   genre, release_date, record_label)
+VALUES
+('North', '2', 'Sango',
+  'Electronic', '2013-07-23', 'Soulection')
+
